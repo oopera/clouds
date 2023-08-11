32faf0113e15efaea15804d685942c5ad615d3cb
Oopera <ll@lucaslichner.de>
2023-08-12 00:48:29 +0200

    refactoring?

--- src/lib/shaders/shaders/cloudShader.ts
+++ src/lib/shaders/shaders/cloudShader.ts
@@ -6,8 +6,6 @@ struct Uniforms {
   cameraPosition : vec4<f32>,
 };
 
-// unpopulated except radius
-
 struct CloudUniforms {
   radius : f32,
   coverage : f32, 
@@ -44,19 +42,19 @@ struct Output {
 
  
 @vertex fn vs(input: Input, @builtin(vertex_index) vertexIndex: u32) -> Output {
-  var output: Output;
+    var output: Output;
 
-  let mPosition: vec4<f32> = uni.modelMatrix * input.position;
-  let mNormal: vec4<f32> = uni.normalMatrix * input.normal;
-  let displacement: vec4<f32> = vec4<f32>(normalize(mPosition.xyz) * (cloudUniforms.radius), 0.0);
-  
-  output.Position = uni.viewProjectionMatrix * (mPosition + displacement);
-  output.vPosition = uni.viewProjectionMatrix * (mPosition + displacement);
-  output.vNormal = mNormal;
-  output.vUV = input.uv;
-  
+    let mPosition: vec4<f32> = uni.modelMatrix * input.position;
+    let mNormal: vec4<f32> = uni.normalMatrix * input.normal;
 
-  return output;
+    let displacement: vec4<f32> = vec4<f32>(normalize(mPosition.xyz) * (cloudUniforms.radius), 0.0);
+    
+    output.Position = uni.viewProjectionMatrix * (mPosition + displacement);
+    output.vPosition = uni.viewProjectionMatrix * (mPosition + displacement);
+    output.vNormal = mNormal;
+    output.vUV = input.uv;
+
+    return output;
 }
 
 fn blend(baseColor: vec3<f32>, newColor: vec3<f32>, light: f32) -> vec3<f32> {
@@ -64,7 +62,7 @@ fn blend(baseColor: vec3<f32>, newColor: vec3<f32>, light: f32) -> vec3<f32> {
 }
 
 fn getNoise(p: vec3<f32>) -> vec4<f32> {
-  let noiseScale = vec3<f32>(1.0, 1.0, 1.0);
+  let noiseScale = vec3<f32>(2.5, 2.5,2.5);
   
    let noise = textureSample(noise_texture, noise_sampler, p * noiseScale);
    return noise;
@@ -78,36 +76,57 @@ fn getCoverage(p: vec3<f32>) -> f32 {
 
   return textureSample(texture, textureSampler, vec2<f32>(longitude, latitude)).r;
 }
-  
+
+fn smoothstep(a: f32, b: f32, x: f32) -> f32 {
+  let t = clamp((x - a) / (b - a), 0.0, 1.0);
+  return t * t * (3.0 - 2.0 * t);
+}
+
+fn clamp(x: f32, minVal: f32, maxVal: f32) -> f32 {
+  return max(min(x, maxVal), minVal);
+}
+
 
 @fragment fn fs(output: Output) -> @location(0) vec4<f32> {
   let cameraPosition: vec3<f32> = uni.cameraPosition.rgb;
   var rayOrigin: vec3<f32> = output.vNormal.xyz;
   var rayDirection: vec3<f32> = normalize(rayOrigin + cameraPosition);
-  
+
+  var sunRayDirection: vec3<f32> = normalize(rayOrigin + lightUni.lightPosition);
+  var sunDensity: f32 = 0.0;
+
   var density: f32 = 0.0;
   var noise : vec4<f32>;
   var coverage: f32;
   var color : vec4<f32> = vec4<f32>(0.0, 0.0, 0.0, 1.0);
 
-  let startDepth: f32 = cloudUniforms.radius / 10;
-  let endDepth: f32 = cloudUniforms.radius;
+  var OGNoise = getNoise(rayOrigin).b / 25;
 
-  let stepSize: f32 = cloudUniforms.radius / 50;
+  let startDepth: f32 = -1 * (cloudUniforms.radius) / 2; 
+  let endDepth: f32 = -1 * (cloudUniforms.radius / 100); 
+  let stepSize: f32 = cloudUniforms.radius / 50; 
 
 
   for (var depth: f32 = startDepth; depth < endDepth; depth += stepSize) {
-    // rayDirection = normalize(rayOrigin + cameraPosition);
+    rayDirection = normalize(rayOrigin + cameraPosition);
     let texturePosition: vec3<f32> = rayOrigin + rayDirection * depth;
 
     noise = getNoise(texturePosition);
     coverage = getCoverage(texturePosition);
 
-    density += coverage * noise.b;
+    density += coverage;
+
+    // for (var depth: f32 = startDepth; depth < endDepth / 10; depth += stepSize) {
+    //   sunRayDirection = normalize(rayOrigin + lightUni.lightPosition);
+    //   let sunTexturePosition: vec3<f32> = rayOrigin + rayDirection * depth;
+  
+    //   coverage = getCoverage(texturePosition) * noise.b;
+    //   sunDensity += coverage;
+    // }
 
     // falloff = smoothstep(min, falloff, 1, distance + noise);
 
-    let baseColor = vec3<f32>(0.54, 0.57, 0.52);  
+    let baseColor = vec3<f32>(0.14, 0.17, 0.22);  
     let highColor = vec3<f32>(0.84, 0.87, 0.92); 
 
     var cloudColor: vec3<f32> = mix(baseColor, highColor, density);
@@ -115,16 +134,17 @@ fn getCoverage(p: vec3<f32>) -> f32 {
     color.r = cloudColor.r;
     color.g = cloudColor.g;
     color.b = cloudColor.b;
-
   
     rayOrigin = texturePosition;
   }
+  
+  var outputDensity: f32 = density + sunDensity;
 
   var lightDirection: vec3<f32> = normalize(lightUni.lightPosition - output.vPosition.xyz);
   var lightIntensity: f32 = max(dot(output.vNormal.xyz, lightDirection), 0.0);
-   color = vec4<f32>(blend(vec3<f32>(0.0, 0.0, 0.0), vec3<f32>(1.0, 1.0, 1.0), density), density);
+  color = vec4<f32>(blend(vec3<f32>(0.0, 0.0, 0.0), vec3<f32>(1.0, 1.0, 1.0), outputDensity), outputDensity);
   
-   if(color.a < 0.1) {
+  if(color.a < 0.1) {
     discard;
   }
 
@@ -136,8 +156,11 @@ fn getCoverage(p: vec3<f32>) -> f32 {
     lightness = 0.25;
   }
 
+  if(coverage < 1){
+    return vec4(color.r, color.g, color.b, color.a + 0.25) * lightness * noise.b;
+  }
 
-  return vec4(color.r * noise.a * 0.80, color.g * noise.a * 0.82, color.b * noise.a * 0.92, color.a * noise.a + 0.25) * lightness;
+  return vec4(color.r, color.g, color.b, color.a + 0.25) * lightness * noise.g;
 
 }
 `;
