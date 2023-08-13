78be731fc8946db91ebbcba3963c5086373bf218
Oopera <ll@lucaslichner.de>
2023-08-13 23:57:51 +0200

    Improve lighting

--- src/lib/shaders/shaders/cloudShader.ts
+++ src/lib/shaders/shaders/cloudShader.ts
@@ -47,29 +47,8 @@ struct Output {
     let mPosition: vec4<f32> = uni.modelMatrix * input.position;
     let mNormal: vec4<f32> = uni.normalMatrix * input.normal;
 
-    var d: vec2<i32> = vec2<i32>(textureDimensions(texture));
-
-    var heightPixel: vec4<f32> = textureLoad(
-      texture,
-      vec2<i32>(i32(input.uv.x * f32(d.x)), i32(input.uv.y * f32(d.y))),
-      0
-    );
-    var height: f32 = heightPixel.x;
-
-    var noisePixel: vec4<f32> = textureLoad(
-      noise_texture,
-      vec3<i32>(i32(input.uv.x * f32(d.x)), i32(input.uv.y * f32(d.y)), 0),
-      0
-    );
-
     var displacement:vec4<f32> = vec4<f32>(normalize(mPosition.xyz) * (cloudUniforms.radius), 0.0);
-    // if(height < 0.25) {
-    // }else if(height < 0.5){
-    //   displacement = vec4<f32>(normalize(mPosition.xyz) * (cloudUniforms.radius + noisePixel.r / 30), 0.0);
-    // }else{
-    //   displacement = vec4<f32>(normalize(mPosition.xyz) * (cloudUniforms.radius + noisePixel.b / 30), 0.0);
-    // }
-    
+
     output.Position = uni.viewProjectionMatrix * (mPosition + displacement);
     output.vPosition = mPosition;
     output.vNormal = mNormal;
@@ -105,24 +84,6 @@ fn clamp(x: f32, minVal: f32, maxVal: f32) -> f32 {
   return max(min(x, maxVal), minVal);
 }
 
-fn computeLighting(density: f32, depth: f32, maxDepth: f32, cosTheta: f32) -> f32 {
-  let scaledMaxDepth: f32 = 1.0;
-  let depthScaleFactor: f32 = scaledMaxDepth / maxDepth;
-  let scaledDepth: f32 = depth * depthScaleFactor;
-
-  let light: f32 = density * (depthScaleFactor - scaledDepth);
-  let scatteredLight = light * schlickPhase(0.0, cosTheta);
-  
-  return scatteredLight;
-}
-
-fn schlickPhase(g: f32, cosTheta: f32) -> f32 {
-  let g2 = g * g;
-  let denom = 1.0 + g2 - 2.0 * g * cosTheta;
-  return (4.0 - g2) / (1.0 * 3.141592653589793238 * denom * sqrt(denom));
-}
-
-
 @fragment fn fs(output: Output) -> @location(0) vec4<f32> {
   let cameraPosition: vec3<f32> = uni.cameraPosition.rgb;
   var rayOrigin: vec3<f32> = output.vPosition.xyz - cloudUniforms.radius * cameraPosition;
@@ -130,15 +91,6 @@ fn schlickPhase(g: f32, cosTheta: f32) -> f32 {
 
   var sunRayDirection: vec3<f32> = normalize(rayOrigin + lightUni.lightPosition);
   var sunDensity: f32 = 0.0;
-
-  let dotProduct = dot(lightUni.lightPosition, output.vNormal.xyz);
-  let scaledDotProduct: f32 = dotProduct * 10.0;
-  var lightness: f32 = 1.0 - (1.0 / (1.0 + exp(-scaledDotProduct)));
-
-  if(lightness < 0.5) {
-    lightness = 0.5;
-  }
-
 
   var density: f32 = 0.0;
   var noise : vec4<f32>;
@@ -152,9 +104,9 @@ fn schlickPhase(g: f32, cosTheta: f32) -> f32 {
   var color : vec4<f32> = vec4<f32>(0.0, 0.0, 0.0, 1.0);
   var light: f32 = 1.0;
 
-  let stepSize: f32 = 0.000001; 
-  let startDepth: f32 = (cloudUniforms.radius * 0.95) ; 
-  let endDepth: f32 =  (cloudUniforms.radius * 0.95) + (50 * stepSize); 
+  let stepSize: f32 = 0.0001; 
+  let startDepth: f32 = cloudUniforms.radius; 
+  let endDepth: f32 =  startDepth + (40 * stepSize); 
 
   let baseColor = vec3<f32>(0.14, 0.17, 0.22);  
   let highColor = vec3<f32>(0.84, 0.87, 0.92); 
@@ -164,9 +116,7 @@ fn schlickPhase(g: f32, cosTheta: f32) -> f32 {
 
   for (var depth: f32 = startDepth; depth < endDepth; depth += stepSize) {
     rayDirection = normalize(rayOrigin + cameraPosition);
-    let texturePosition: vec3<f32> = rayOrigin + rayDirection * depth ;
-
-    light = clamp(computeLighting(density, depth, endDepth, dot(rayDirection, output.vNormal.xyz)), 0.0, 1.0);
+    let texturePosition: vec3<f32> = rayOrigin + rayDirection * depth;
 
     noise = getNoise(texturePosition, vec3<f32>(1.0, 1.0,1.0));
 
@@ -177,22 +127,22 @@ fn schlickPhase(g: f32, cosTheta: f32) -> f32 {
 
     coverage = getCoverage(texturePosition);
 
-    if(coverage < 1){
-      density += (endDepth /depth) * perlin * coverage  / 500;
+    if(coverage < .75){
+      density += (endDepth /depth) * perlin * coverage  / 350;
     }else{
-    density += (endDepth /depth) * worley_l * coverage  / 200;
+    density += (endDepth /depth) * billowy * coverage  / 200;
     }
     
-    // for (var depth: f32 = -1 * startDepth; depth < -1 * endDepth; depth -= stepSize * 25) {
+    // for (var depth: f32 = startDepth; depth < endDepth; depth += stepSize * 5 ) {
     //   sunRayDirection = normalize(rayOrigin + lightUni.lightPosition);
     //   let sunTexturePosition: vec3<f32> = rayOrigin + sunRayDirection * depth;
   
     //   coverage = getCoverage(sunTexturePosition);
 
-    //   if(coverage < 1){
-    //     sunDensity += (endDepth /depth) * worley_s * coverage / 500 * light;
+    //   if(coverage < .75){
+    //     sunDensity += ( endDepth - startDepth / depth) * worley_s * coverage / 500 ;
     //   }else{
-    //     sunDensity += (endDepth /depth) * billowy * coverage  / 200 * light;
+    //     sunDensity += (endDepth - startDepth / depth) * billowy * coverage  / 200 ;
     //   }
     // }
 
@@ -201,15 +151,28 @@ fn schlickPhase(g: f32, cosTheta: f32) -> f32 {
     rayOrigin = texturePosition;
   }
 
-  outputDensity = clamp(outputDensity, 0.0, 1.0);
+  let dotProduct = dot(lightUni.lightPosition, output.vNormal.xyz);
+  let scaledDotProduct: f32 = dotProduct * 10.0;
+  var lightness: f32 = 1.0 - (1.0 / (1.0 + exp(-scaledDotProduct)));
+  let edge = fwidth(lightness);
+  let borderColor = vec4(0.7, 0.62, 0.65, 1.0);
+  let blendRadius = 0.1; 
+  let mask = smoothstep(0.0, blendRadius, edge);
 
+  outputDensity = clamp(outputDensity, 0.0, 1.0);
   color = vec4<f32>(blend(baseColor, highColor, outputDensity), outputDensity);
 
-  if(coverage < 1){
-    return vec4<f32>(blend(color.rgb, vec3(perlin, perlin,perlin),outputDensity), outputDensity);
+  if(coverage < .75){
+    color =  vec4<f32>(blend(color.rgb, vec3(perlin, perlin,perlin),outputDensity), outputDensity);
+  }else{
+    color = vec4<f32>(blend(color.rgb, vec3(billowy, billowy, billowy), outputDensity), outputDensity);
   }
 
-  return vec4<f32>(blend(color.rgb, vec3(billowy, billowy, billowy), outputDensity), outputDensity);
+  if(lightness < 0.75){
+    lightness = 0.75;
+  }
+
+  return color * lightness + mask * borderColor;
 
 }
 `;
